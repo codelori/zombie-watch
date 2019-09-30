cb078e9abc2a0b678af6917296144f9b5abec900
Loriana Hines <codeloriana@gmail.com>
2019-09-30 18:17:02 -0400

    created react app skeleton

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Header extends React.Component{
+	render(){
+		return(
+			<header>
+				<h1> Zombie Watch </h1>
+				<h4> Stay updated on current zombie outbreaks.</h4>
+			</header>
+		)
+	}
+}
---
+++ src/components/NewsFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class NewsFeed extends React.Component{
+	render(){
+		return(
+			<section className="news-feed">
+				<h3>Zombie Sightings</h3>
+			</section>
+		)
+	}
+}
